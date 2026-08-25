2a9985796729a1fdb940825253840fc2e5ad5e72
Xander Rude <a@rudenko.one>
2026-08-25 13:32:30 +0300

    Добавлен пакет адаптеров карьерных сайтов

---
+++ sources/__init__.py
@@ -0,0 +1,9 @@
+from .base import RawVacancy, SourceResult, VacancySource
+from .yandex import YandexSource
+
+__all__ = [
+    "RawVacancy",
+    "SourceResult",
+    "VacancySource",
+    "YandexSource",
+]
