87f652e3f1a27448c045507783c13a9274fc5624
Xander Rude <a@rudenko.one>
2026-08-26 15:22:40 +0300

    Подключена разбивка очереди approved по источникам

--- telegram_bot_entry.py
+++ telegram_bot_entry.py
@@ -5,11 +5,13 @@ import telegram_cover_letter_patch
 from telegram_bot_link_patch import install as install_link_patch
 from telegram_bot_pending_patch import install as install_pending_patch
 from telegram_cover_letter_output_patch import install as install_cover_output_patch
+from telegram_queue_stats_patch import install as install_queue_stats_patch
 
 
 install_link_patch(telegram_bot)
 install_pending_patch(telegram_bot)
 install_cover_output_patch(telegram_cover_letter_patch)
+install_queue_stats_patch(telegram_bot)
 telegram_cover_letter_patch.install(telegram_bot)
 
 
