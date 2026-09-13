1f4078e38d97dd00cbe4713e05063db3cc236bf4
Xander Rude <a@rudenko.one>
2026-09-13 16:51:04 +0300

    Install non-blocking Telegram /new patch

--- telegram_bot_entry.py
+++ telegram_bot_entry.py
@@ -42,6 +42,7 @@ from telegram_bot_pending_patch import install as install_pending_patch
 from telegram_cover_letter_output_patch import install as install_cover_output_patch
 from telegram_hard_filter_appeal_patch import install as install_appeal_patch
 from telegram_heartbeat_patch import install as install_heartbeat_patch
+from telegram_new_background_patch import install as install_new_background_patch
 from telegram_queue_stats_patch import install as install_queue_stats_patch
 from targeted_hunt_telegram_patch import install as install_targeted_hunt_patch
 
@@ -51,6 +52,7 @@ NETWORK_RETRY_DELAY_SECONDS = 30
 
 install_link_patch(telegram_bot)
 install_pending_patch(telegram_bot)
+install_new_background_patch(telegram_bot)
 install_cover_output_patch(telegram_cover_letter_patch)
 install_queue_stats_patch(telegram_bot)
 telegram_cover_letter_patch.install(telegram_bot)
